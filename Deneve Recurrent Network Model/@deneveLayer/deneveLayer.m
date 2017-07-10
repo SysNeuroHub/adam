4796758ca2c199da93e3734fb82755cdd4e34e4f
cabad1 <cabad1@student.monash.edu>
2017-07-10 16:45:17 +1000

    Improved version, but currently broken! It runs, but sampling the world is wrong.

--- Deneve Recurrent Network Model/@deneveLayer/deneveLayer.m
+++ Deneve Recurrent Network Model/@deneveLayer/deneveLayer.m
@@ -1,25 +1,32 @@
 classdef deneveLayer < handle
     
     properties
+    %for ron    
+    end
+    
+    properties (SetAccess = protected)
         name@char;
-        inLayer@deneveLayer;    %The handle(s) of the input layer(s)
-        w@cell;                 %Cell array of weight vectors/matrices, one for each input layer, used to pool the activity of its input layer
-        r@double;               %Activity of each unit       
+        input@struct;              %o.input will be a structure, with a handle, a set of weights, and a switch
+        resp@double;               %Activity of each unit (response of each neuron) in the population at a given time point
+        
     end
     
     properties (Dependent)
-        size;
-        nInLayers;
-        nUnits;
+        size;                      %Size of response matrix
+        nInputs;                   %Number of input layers
+        nUnits;                    %Number of units in the response matrix
     end
     
     methods
+        %get size of response matrix
         function sz = get.size(o)
-            sz = size(o.r); %#ok<CPROP>
+            sz = size(o.resp);
         end
-        function n = get.nInLayers(o)
-            n = numel(o.inLayer);
+        %get number of input layers
+        function n = get.nInputs(o)
+            n = numel(o.input);
         end
+        %get number of units in the response matrix
         function n = get.nUnits(o)
             n = prod(o.size);
         end
@@ -29,45 +36,118 @@ classdef deneveLayer < handle
         function o = deneveLayer(name, dim1,dim2)
             %Constructor.
             o.name = name;
-            o.r = zeros(dim1,dim2);
+            o.resp = zeros(dim1,dim2);
         end
         
-        function o = addInput(o,layer)
-            %Add one (or more) input layers that feed this layer
-            o.inLayer = horzcat(o.inLayer,layer);
+        function o = setInput(o,layer,weights,varargin)
+            %Help here.
+            %This is to ensure that the input is in the correct format, and
+            %allow for multiple fields after the layers and weights
+            p=inputParser;
+            p.addRequired('layer',@iscell);
+            p.addRequired('weights',@iscell);
+            p.addParameter('enabled',[], @iscell);
+            p.parse(layer,weights,varargin{:});
+            
+            %Extract fields from input parser
+            layer = p.Results.layer;
+            weights = p.Results.weights;
+            enabled = p.Results.enabled;
+            
+            %Fill the enabled field if left blank, defaults to enabled
+            if isempty(enabled)
+                enabled = true(1,numel(layer));
+            end
+            
+            
+            %Fill values into input structure
+            nln=numel(layer);
+            for i=1:nln;
+                o.input(i).layer=layer{i};
+                o.input(i).weights=weights{i};
+                o.input(i).enabled=enabled(i);
+            end
         end
         
         function o = initialise(o,r)
             %Set the initial state of the layer (i.e. activity of each unit)
-            o.r = r;
+            o.resp = r;
         end
         
-        function o = update(o)
+        function o = reset(o,dim1,dim2)
+            %Reset the network responses to zero between simulations
+            o.resp = zeros(dim1,dim2);
+        end
+        
+        function o = addNoise(o,i)
+            %add noise to the response
+            o.resp(i) = poissrnd(o.resp(i));
+        end
+        
+        function setEnabled(o,inputName,enabled)
+            %Allows enabling and disableing of input layers
+            thisInput = arrayfun(@(x) strcmpi(x.layer.name,inputName),o.input);
+            o.input(thisInput).enabled = enabled;
+        end
+        
+        function o = update(o,normalise)
+            %Can switch of normalisation, defaults to on
+            if nargin < 2
+                normalise = true;
+            end
+            
             %Pool the input from this layer's input layer(s)
             o = poolInput(o);
             
             %Perform squaring and divisive normalisation
-            o = transfer(o);
+            if normalise
+                o = transfer(o);
+            end
         end
         
-        function o = poolInput(o)     
+        function o = poolInput(o)
             %Pool the input to each unit, across all input layers (o.inLayers is > 1 only for the hidden layer)
             %Uses linear indexing so it can be blind to whether this is a 1D or 2D layer
             rIn = zeros(o.size);
-            for u=1:o.nUnits
-                for i=1:o.nInLayers
-                    thisPool = o.w{u}{i}.*o.inLayer(i).r;
-                    rIn(u) = rIn(u) + sum(thisPool(:));
+            
+            for i=1:o.nInputs
+                if o.input(i).enabled
+                    %Only includes input if enabled
+                    w = o.input(i).weights;
+                    r = o.input(i).layer.resp;
+                    inDims = ndims(r)-isvector(r);
+                    sz = size(w);
+                    %Reshape to a 2D matrix of weights by unit
+                    w = reshape(w,prod(sz(1:inDims)),[]);
+                    
+                    for u=1:o.nUnits
+                        %Sum response for a given 'neuron'
+                        rIn(u) = rIn(u) + w(:,u)'*r(:);
+                    end
                 end
             end
-            o.r = rIn;
+            o.resp = rIn;
         end
         
         function o = transfer(o)
+            %Function used to normalise responses
             S = 0.1;
             mu = 0.002;
-            o.r = o.r.^2;
-            o.r = o.r/(S+mu.*sum(o.r(:)));
+            o.resp = o.resp.^2;
+            o.resp = o.resp/(S+mu.*sum(o.resp(:)));
         end
+           function estimate = pointEstimate(o)
+            %Estimate position based on peak of hill
+            %Preallocate matrix
+            estimator = zeros(1,o.nUnits);
+            %Generate matrix to sum
+            for j = 1:o.nUnits
+                estimator(j) = o.resp(j).*exp(1i.*((2*pi)/o.nUnits).*j);
+            end
+                      
+            %Calculate estimate
+            e = sum(estimator);
+            estimate = mod(phase(e)/(2*pi)*o.nUnits,o.nUnits);
+        end   
     end
 end
--- Deneve Recurrent Network Model/deneve.m
+++ Deneve Recurrent Network Model/deneve.m
@@ -1,84 +1,168 @@
 function deneve
+%Set constants for functions and loops
 nIter = 50;
-N=20;
+N = 20;
 Kw = 1;
 sigmaw = 0.37;
+K = 20;
+v = 1;
+sigma = 0.40;
 
 %Create layers ("population codes"). All layers are instances of the deneveLayer class.
-hid = deneveLayer('hidden',N,N);    %Hidden layer
-ret = deneveLayer('retinal',1,N);   %Object location on the retina
-eye = deneveLayer('eye',1,N);       %Eye position signal
-hed = deneveLayer('head',1,N);      %Object location relative to the head
+%Network layers
+net.hid = deneveLayer('hidden',N,N);    %Hidden layer
+net.ret = deneveLayer('retinal',1,N);   %Object location on the retina
+net.eye = deneveLayer('eye',1,N);       %Eye position signal
+net.hed = deneveLayer('head',1,N);      %Object location relative to the head
 
-%Specify the network architecture (who talks to whom?)
-hid.addInput([ret,eye,hed]);
-ret.addInput(hid);
-eye.addInput(hid);
-hed.addInput(hid);
+%World layers
+wld.ret = deneveLayer('retinalWorld',1,N);  %Layer to represent world location in retinal coordinates
+wld.eye = deneveLayer('eyeWorld',1,N);      %Layer to represent world location in eye centred coordinates
+%Add wld.hed layer here
 
-%Set the input weights for each layer. Weights are symmetric, such that the
-%input weight from neuron A to be is the same as the input weight from B to A
+%Set the input weights for each network layer. Weights are symmetric, such that the
+%input weight from neuron A to B is the same as the input weight from B to A
+%weights are stored in temporary variables to be used in inputs
+%Function for setting weights could eventually go in class
+%Such that weight function is an argument
 wfun = @(ind) Kw.*exp((cos((2*pi/N).*ind)-1)/sigmaw^2); %Anonymous function for the bell-shaped input weights
+%Preallocate matrices with dimensions [N,N]
+tempretw = zeros(N,N,N);
+tempeyew = zeros(N,N,N);
+temphedw = zeros(N,N,N);
 for j=1:N
     for l=1:N
         for m=1:N
-            %Pooling weights for each unit in the input/output layers (i.e. an N x N matrix)
-            ret.w{j}{1}(l,m) = wfun(j-l);
-            eye.w{j}{1}(l,m) = wfun(j-m);
-            hed.w{j}{1}(l,m) = wfun(j-l-m);
+            %Pooling weights for each unit in the input/output layers (i.e. an N x N x N matrix)
+            tempretw(l,m,j) = wfun(j-l);
+            tempeyew(l,m,j) = wfun(j-m);
+            temphedw(l,m,j) = wfun(j-l-m);
             
-            %Pooling weights for each hidden unit  (i.e. an 1 x N matrix for each input layer)
-            hid.w{l,m}{1}(j) = ret.w{j}{1}(l,m);
-            hid.w{l,m}{2}(j) = eye.w{j}{1}(l,m);
-            hid.w{l,m}{3}(j) = hed.w{j}{1}(l,m);
+            %Pooling weights for each hidden unit  (i.e. an N x N x N matrix for each input layer)
+            temphidw{1}(j,l,m) = tempretw(l,m,j);
+            temphidw{2}(j,l,m) = tempeyew(l,m,j);
+            temphidw{3}(j,l,m) = temphedw(l,m,j);
         end
     end
 end
 
+%Calculate input weights for world.  Communication between the eye and ret
+%layers with the world are feedforward from the world to the network
+%Weights are based on the circular Von Mises function
+vmfun = @(pos,unit) K.*exp((cos(pos-(2*pi/N).*unit)-1)/(sigma^2))+v; %Anon function for world to network weights
+%Preallocate matrices to size [N,N]
+tempWldRet = zeros(N,N);
+tempWldEye = zeros(N,N);
+for i=1:N
+    for j=1:N
+       tempWldRet(i,j)= vmfun(i,j);
+       tempWldEye(i,j)= vmfun(i,j);
+       %Add tempWorldHed here
+    end 
+end
+
+
+%Specify the network architecture (who talks to whom?)
+%keep order of inputs consistent across network inputs
+net.hid.setInput({net.ret,net.eye,net.hed},temphidw);
+net.ret.setInput({net.hid,wld.ret},{tempretw,tempWldRet});
+net.eye.setInput({net.hid,wld.eye},{tempeyew,tempWldEye});
+net.hed.setInput({net.hid},{temphedw});    %Add wld.hed input here
+
 %=========== Run the simulation ==============
 nSims = 20;
+%Preallocating vectors for true and estimate locations
 for s = 1:nSims
-    %Initialise with random numbers
-    ret.initialise(poissrnd(50,1,N));
-    eye.initialise(poissrnd(50,1,N));
-    hed.initialise(zeros(1,N));
-    hid.initialise(zeros(N,N));
-    eye.r(randi(N))=100;
-    ret.r(randi(N))=100;
+    %Initialise world layers with random delta functions
+    r = zeros(1,N);
+    realRetPos = randi(N);
+    r(realRetPos)= 1;
+    wld.ret.initialise(r);
+    r = zeros(1,N);
+    realEyePos = randi(N);
+    r(realEyePos)= 1;
+    wld.eye.initialise(r);
+    %Add wld.hedinitialise and wld.hed.resp here
+    
+    %Record real positions in vectors
+    truRet(s) = realRetPos;
+    truEye(s) = realEyePos;
+    
+    %Reset network layers to zero for each simulation
+    net.ret.reset(1,N);
+    net.eye.reset(1,N);
+    net.hed.reset(1,N);
+    net.hid.reset(N,N);
+    
     for t=1:nIter
-        plotState(ret,eye,hed,hid,t);
+        %Switch between world and hidden layer inputs for ret, eye and hed
+        isFirstTime=t==1;
+        setEnabled(net.ret,net.hid.name,~isFirstTime);
+        setEnabled(net.ret,wld.ret.name,isFirstTime);
+        setEnabled(net.eye,net.hid.name,~isFirstTime);
+        setEnabled(net.eye,wld.eye.name,isFirstTime);
+        %Add hed.input here
         
-        hid.update();
-        ret.update();
-        eye.update();
-        hed.update();
+        %Update for each time point to include recurrent inputs
+        net.hid.update(~isFirstTime);
+        net.ret.update(~isFirstTime);
+        net.eye.update(~isFirstTime);
+        net.hed.update(~isFirstTime);
+        
+        %Add noise to the response of ret and eye networks at the first
+        %time point, from the world input
+%         if isFirstTime
+%             for i = 1:N
+%                 addNoise(net.ret,i);
+%                 addNoise(net.eye,i);
+%                 %Add addNoise(net.hed,i)
+%             end
+%         end
+        
+        %Plot the simulation
+        plotState(net,t);
     end
+    
+    %Vectors of peak estimates
+    estRet(s) = pointEstimate(net.ret);
+    estEye(s) = pointEstimate(net.eye);
+    estHed(s) = pointEstimate(net.hed);
 end
 keyboard;
 
-function plotState(ret,eye,hed,hid,t)
+%====Function to plot the simulation====
 
+function plotState(net,t)
+%add a wld input eventually
+
+%Plot ret, eye, and hed responses on the same subplot
 subplot(2,1,1);
 cla
-plot(ret.r,'r-o','linewidth',4);
+plot(net.ret.resp,'r-o','linewidth',4);
 hold on;
-plot(eye.r,'b-o','linewidth',4);
-plot(hed.r,'g-o','linewidth',4);
+plot(net.eye.resp,'b-o','linewidth',4);
+plot(net.hed.resp,'g-o','linewidth',4);
 
-[maxVal,retPos] = max(ret.r);
+%Add line to indicate peak of ret, eye, and hed networks
+%Change to point estimate function (within class) from supp material
+[maxVal,retPos] = max(net.ret.resp);
 plot([retPos retPos],[0 maxVal],'r','linewidth',3);
-[maxVal,eyePos] = max(eye.r);
+[maxVal,eyePos] = max(net.eye.resp);
 plot([eyePos eyePos],[0 maxVal],'b','linewidth',3);
-[maxVal,headPos] = max(hed.r);
+[maxVal,headPos] = max(net.hed.resp);
 plot([headPos headPos],[0 maxVal],'g','linewidth',3);
-plot(mod([retPos+eyePos retPos+eyePos],ret.nUnits),[0 maxVal],'k:','linewidth',3);
+%Add line to indicate point satisfying hed=ret+eye rule
+plot(mod([retPos+eyePos retPos+eyePos],net.ret.nUnits),[0 maxVal],'k:','linewidth',3);
 ylim([0,100]);
 
+%Plot hidden layer on seperate subplot
 subplot(2,1,2);
 cla
-surf(hid.r); zlim([0,10])
+surf(net.hid.resp); zlim([0,10])
+
+%Timing of simulation
 if t==1
-    pause(3);
+    pause(2);
 else
     pause(0.15);
 end%2./t);
